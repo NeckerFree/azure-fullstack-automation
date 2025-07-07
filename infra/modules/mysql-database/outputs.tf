554f03a3c46c772f29bde86a1e5f91dd9cf1710f
Elio Cortés <elio.cortes3000@gmail.com>
2025-07-07 12:30:07 -0500

    updated app and api setup

--- infra/modules/mysql-database/outputs.tf
+++ infra/modules/mysql-database/outputs.tf
@@ -10,6 +10,13 @@ output "mysql_admin_user" {
   sensitive   = true
 }
 
+output "mysql_admin_pwd" {
+  value       = azurerm_mysql_flexible_server.mysql.administrator_password
+  description = "MySQL admin password"
+  sensitive   = true
+}
+
+
 output "mysql_database_name" {
   value       = azurerm_mysql_flexible_database.movie_analyst.name
   description = "MySQL database name"
