540aa757319dea64ae26fc64b044e59521258b8e
Elio Cortés <elio.cortes3000@gmail.com>
2025-07-09 19:47:12 -0500

    added mysql_server section

--- ansible/deploy-db-from-local.sh
+++ ansible/deploy-db-from-local.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-# 🧠 Terraform outputs
-echo "[0/5] Obteniendo outputs desde Terraform..."
-MYSQL_HOST=$(terraform output -raw mysql_fqdn)
-MYSQL_USER=$(terraform output -raw mysql_admin_user)
-MYSQL_PWD=$(terraform output -raw mysql_admin_pwd)
-MYSQL_DB=$(terraform output -raw mysql_database_name)
+# 📥 Obtener outputs de Terraform localmente
+echo "[0/4] Obteniendo outputs desde Terraform..."
+MYSQL_HOST=$(terraform -chdir=infra/modules/mysql-database output -raw mysql_fqdn)
+MYSQL_USER=$(terraform -chdir=infra/modules/mysql-database output -raw mysql_admin_user)
+MYSQL_PWD=$(terraform -chdir=infra/modules/mysql-database output -raw mysql_admin_pwd)
+MYSQL_DB=$(terraform -chdir=infra/modules/mysql-database output -raw mysql_database_name)
 
 # 📡 SSH y rutas
 JUMPBOX_IP="4.155.204.253"
@@ -17,21 +17,20 @@ PLAYBOOK="ansible/db-setup.yml"
 SQL_SCRIPT="ansible/files/mysql/movie_db.sql"
 INVENTORY="ansible/inventory.ini"
 
-# 📤 Envío de archivos
-echo "[1/4] Copiando playbook y script SQL a la jumphost..."
-scp -i "$SSH_KEY" "$PLAYBOOK" "$SQL_SCRIPT" "$JUMPBOX_USER@$JUMPBOX_IP:/home/$JUMPBOX_USER/"
+# 📤 Enviar archivos a la jumphost
+echo "[1/3] Copiando playbook, SQL script e inventario a la jumphost..."
+scp -i "$SSH_KEY_LOCAL" "$PLAYBOOK" "$SQL_SCRIPT" "$INVENTORY" "$JUMPBOX_USER@$JUMPBOX_IP:/home/$JUMPBOX_USER/"
 
-echo "[2/4] Copiando archivo de inventario..."
-scp -i "$SSH_KEY" "$INVENTORY" "$JUMPBOX_USER@$JUMPBOX_IP:/home/$JUMPBOX_USER/"
-
-# 🧪 Ejecución remota con Ansible
-echo "[3/4] Ejecutando playbook desde la jumphost..."
-ssh -i "$SSH_KEY" "$JUMPBOX_USER@$JUMPBOX_IP" bash <<EOF
+# 🧪 Ejecutar playbook remotamente
+echo "[2/3] Ejecutando playbook desde la jumphost..."
+ssh -i "$SSH_KEY_LOCAL" "$JUMPBOX_USER@$JUMPBOX_IP" bash <<EOF
   set -e
   sudo apt update -qq
-  sudo apt install -y ansible
+  sudo apt install -y ansible mysql-client
   ansible-playbook -i inventory.ini db-setup.yml \
     --extra-vars "db_user=$MYSQL_USER db_password=$MYSQL_PWD db_name=$MYSQL_DB db_host=$MYSQL_HOST"
 EOF
 
-echo "[4/4] ¡Playbook ejecutado exitosamente en la jumphost!"
+echo "[3/3] ¡Playbook ejecutado exitosamente!"
+
+
