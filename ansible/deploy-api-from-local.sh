b1cf7f4e58ad32b3c5a6ff7822b82057c1c04780
Elio Cortés <elio.cortes3000@gmail.com>
2025-07-11 00:10:46 -0500

    fixed copy files error

--- ansible/deploy-api-from-local.sh
+++ ansible/deploy-api-from-local.sh
@@ -6,17 +6,17 @@ JUMP_USER="adminuser"
 JUMP_HOST="4.154.243.88"
 SSH_KEY_LOCAL="$HOME/.ssh/vm_ssh_key"
 REMOTE_DIR="/home/${JUMP_USER}/ansible-setup"
-API_SRC_LOCAL="./src"
-API_SRC_REMOTE="${REMOTE_DIR}/src"
+API_SRC_LOCAL="./src/movie-analyst-api"
+API_SRC_REMOTE="${REMOTE_DIR}/src/movie-analyst-api"
 API_PLAYBOOK_LOCAL="./ansible/api-setup.yml"
 TEMPLATE_LOCAL="./ansible/templates/movie-api.service.j2"
 TEMPLATE_REMOTE="${REMOTE_DIR}/templates/movie-api.service.j2"
 INVENTORY_LOCAL="./ansible/inventory.ini"
 INVENTORY_REMOTE="${REMOTE_DIR}/inventory.ini"
 
-# === STEP 0: JUMPBOX_HOST, JUMPBOX_USER validation===
-if [ -z "$JUMPBOX_HOST" ] || [ -z "$JUMPBOX_USER" ]; then
-  echo "❌ JUMPBOX_HOST or JUMPBOX_USER not defined"
+# === STEP 0: Validate jumpbox configuration ===
+if [ -z "$JUMP_USER" ] || [ -z "$JUMP_HOST" ]; then
+  echo "❌ JUMP_USER or JUMP_HOST not defined"
   exit 1
 fi
 
@@ -24,17 +24,17 @@ fi
 echo "[1/4] Uploading playbook and inventory..."
 scp -i "${SSH_KEY_LOCAL}" "${API_PLAYBOOK_LOCAL}" "${INVENTORY_LOCAL}" "${JUMP_USER}@${JUMP_HOST}:${REMOTE_DIR}/"
 
-# [2/4] Copiar código fuente de la API
-echo "[2/4] Copiando código fuente de la API..."
-scp -i "$SSH_KEY_LOCAL" -r ./src/movie-analyst-api "$JUMPBOX_USER@$JUMPBOX_HOST:/home/$JUMPBOX_USER/ansible-setup/src/"
+# === STEP 2: Copy API source code ===
+echo "[2/4] Copying API source code..."
+ssh -i "$SSH_KEY_LOCAL" "$JUMP_USER@$JUMP_HOST" "mkdir -p ${REMOTE_DIR}/src"
+scp -i "$SSH_KEY_LOCAL" -r "$API_SRC_LOCAL" "$JUMP_USER@$JUMP_HOST:${REMOTE_DIR}/src/"
 
-
-# === STEP 2: Upload systemd template ===
+# === STEP 3: Upload systemd template ===
 echo "[3/4] Uploading systemd template..."
 ssh -i "${SSH_KEY_LOCAL}" "${JUMP_USER}@${JUMP_HOST}" "mkdir -p ${REMOTE_DIR}/templates"
 scp -i "${SSH_KEY_LOCAL}" "${TEMPLATE_LOCAL}" "${JUMP_USER}@${JUMP_HOST}:${TEMPLATE_REMOTE}"
 
-# === STEP 3: Execute playbook remotely ===
+# === STEP 4: Execute playbook remotely ===
 echo "[4/4] Executing playbook from the jump host..."
 ssh -i "${SSH_KEY_LOCAL}" "${JUMP_USER}@${JUMP_HOST}" << EOF
   set -e
